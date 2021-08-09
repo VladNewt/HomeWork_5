46918cc0aaba55430073c9e4176e26febd13992b
VladNewt <cocox2015@yandex.ru>
2021-08-09 23:09:41 +0500

    My home work0908

--- src/homework/Zadaniye_1.java
+++ src/homework/Zadaniye_1.java
@@ -1,8 +1,44 @@
 package homework;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Zadaniye_1 {
 
     public static void main(String[] args) {
+        int[] num = new int[100];
+        Random rnd = new Random();
+        Random logic = new Random();
 
+        for (int i = 0; i < 100; i++) {
+            if(logic.nextBoolean()) {
+                num[i]=rnd.nextInt(30);
+            } else {
+                num[i]=(-1)*rnd.nextInt(30);
+            }
+            //System.out.println(num[i]);
+        }
+
+        System.out.println("Min num = " + Arrays.stream(num).min());
+        System.out.println("Max num = " + Arrays.stream(num).max());
+
+        //Находим сумму элементов массива
+        int sum = 0;
+        for (int i = 0; i < 100; i++) {
+            sum = sum+num[i];
+        }
+
+
+
+
+        double mid = (5/3);
+        double midDown = Math.floor(sum/num.length);
+        double midUp = Math.ceil(sum/num.length);
+
+        System.out.println("Sum = " + sum);
+        System.out.println("Abs(sum) = " + sum);
+        System.out.printf("Middle = %.5f \n", mid);
+        System.out.printf("MiddleDown = %.1f\n", midDown);
+        System.out.printf("MiddleUp = %.1f", midUp);
     }
 }
