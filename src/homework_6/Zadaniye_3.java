784fb6b050a031aed075aa90c8cc4a84b473445a
VladNewt <cocox2015@yandex.ru>
2021-08-15 21:14:39 +0500

    Home work 15.08.2021, redact in the garden.

--- src/homework_6/Zadaniye_3.java
+++ src/homework_6/Zadaniye_3.java
@@ -4,7 +4,7 @@ public class Zadaniye_3 {
     public static void main(String[] args) {
         String str = "Keep,calm,and,listen,to,ROCK";
         String[] arr = str.split(".");
-
+//
         String newstr = str.join(" ",arr);
 
         System.out.println(newstr);
